dce492deb45c95867bfd0eedc2fa7cbebfe4dba0
marlon-delo <wmcriacao@gmail.com>
2025-03-12 18:56:28 +0000

    programa em funcionamento

--- super trunfo.c
+++ super trunfo.c
@@ -1,1 +1,96 @@
+#include <stdio.h>
+
+int main (){
+// carta 1:
+char estado [20] ;
+char codigo_da_carta [20];
+char cidade [20];
+int populacao [20];
+float pib [20];
+int ponto_turistico[20];
+
+printf("digite seu estado:");
+scanf("%s", estado);
+
+printf("digite o codigo da carta:");
+scanf("%s", codigo_da_carta);
+
+printf("digite a cidade:");
+scanf("%s", cidade);
+
+printf("digite a população:");
+scanf("%d", populacao);
+
+printf("digite o pib:");
+scanf("%f", pib);
+
+printf("digite o numero de pontos turisticos:");
+scanf("%d", ponto_turistico);
+
+printf("cidade 2\n");
+//cidade 2
+char estado2 [20] ;
+char codigo_da_carta2 [20];
+char cidade2 [20];
+int populacao2 [20];
+float pib2 [20];
+int ponto_turistico2[20];
+
+printf("digite seu estado:");
+scanf("%s", estado2);
+
+printf("digite o codigo da carta:");
+scanf("%s", codigo_da_carta2);
+
+printf("digite a cidade:");
+scanf("%s", cidade2);
+
+printf("digite a população:");
+scanf("%d", populacao2);
+
+printf("digite o pib:");
+scanf("%f", pib2);
+
+printf("digite o numero de pontos turisticos:");
+scanf("%d", ponto_turistico2);
+
+return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
